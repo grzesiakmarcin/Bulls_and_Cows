f3f1f3613f6a9f3562e9ada6badcc15006b01b91
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-01-01 22:30:16 +0100

    cos staram sie pisac

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
 
 
+
 //        System.out.println("The secret code is prepared: ****.");
 //        System.out.println("Turn 1. Answer");
 //        System.out.println("1234");
@@ -20,107 +21,32 @@ public class Main {
 //        System.out.println("Grade: 4 bulls.");
 //        System.out.println("Congrats! The secret code is 9876.");
 
+
+
+
+
+    }
+}
+class Gra{
+    public void mainOfGame(){
+
+
         Scanner sc = new Scanner(System.in);
 
-        String input= sc.next();
-
+        String password= sc.next();
         String guessInput=sc.next();
 
-        int[] InputArray= new int[4];
+        int[] passwordArray= new int[4];
         int[] guessArray= new int [4];
-
-        int cows=0;
-        int bulls =0;
-
-        for (int i =0; i<4;i++){
-           InputArray[i]=Integer.parseInt(input.substring(i,i+1));
-        }
-
-        for (int i =0; i<4;i++){
-            guessArray[i]=Integer.parseInt(guessInput.substring(i,i+1));
-        }
-
-
-        for (int i=0; i<4;i++){
-
-            if (InputArray[i]==guessArray[i]){
-                bulls++;
-            }
-
-
-        }
-
-        for (int element: InputArray) {
-            for(int i=0;i<4;i++){
-                if (element==guessArray[i]){
-                    cows++;
-                }
-            }
-
-
-        }
-
-        System.out.println("The number "+guessInput+" contains " +bulls+" bulls and "+ cows+" cows.");
-
-
-
-
-
-
-
-
     }
 }
-class Array{
-
-
-}
-
-class Gra {
-    Scanner sc = new Scanner(System.in);
-
-    String input= sc.next();
-
-    String guessInput=sc.next();
-
-    int[] InputArray= new int[4];
-    int[] guessArray= new int [4];
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public int[] makeAnArray (String ourInput, int[] Array){
-        for (int i =0; i<4;i++){
-            Array[i]=Integer.parseInt(ourInput.substring(i,i+1));
-        }
-        return Array;
-    }
-
-}
-
 class ArrayMaker{
 
-//    public ArrayMaker(int[] output, String wsad) {
-//
-//    }
+  public int[] makeArrayFromString(int[] array, String string){
 
-   static int[] zrobTabel(int[] output, String wsad){
-
-        for (int i =0; i<4;i++){
-            output[i]=Integer.parseInt(wsad.substring(i,i+1));
+        for (int i=0;i<array.length;i++){
+            array[i]=Integer.parseInt(string.substring(i,i+1));
         }
-
-        return output;
-
+        return array;
     }
 }
