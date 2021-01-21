b40b60008ea3ce3005490f816ad775b2f8397ac4
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-01-21 22:16:38 +0100

    Local Date - napisalem najlepsze rozwiazanie na swiecie v2.0

--- Problems (4)/Find all mondays/src/Main.java
+++ Problems (4)/Find all mondays/src/Main.java
@@ -1,3 +1,4 @@
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Scanner;
 
@@ -6,15 +7,15 @@ class Main {
         Scanner sc = new Scanner(System.in);
 
         int year = sc.nextInt();
-        int mont = sc.nextInt();
+        int month = sc.nextInt();
 
 
-        LocalDate one = LocalDate.of(year, mont, 1);
+        LocalDate one = LocalDate.of(year, month, 1);
         LocalDate letsFindMonday = one;
         LocalDate firstMonday = letsFindMonday;
 
         for (int i = 0; i < 7; i++) {
-            if (String.valueOf(letsFindMonday.getDayOfWeek()).equals("MONDAY")) {
+            if (letsFindMonday.getDayOfWeek()==DayOfWeek.MONDAY) {
                 firstMonday = letsFindMonday;
                 break;
             }
@@ -23,13 +24,29 @@ class Main {
         }
 
         for (int i=0 ;i<6;i++){
-            if(firstMonday.getMonthValue()==mont) {
+            if(firstMonday.getMonthValue()==month) {
                 System.out.println(firstMonday);
                firstMonday= firstMonday.plusDays(7);
             }
 
 
         }
+
+/*
+w alternatywnej wirtualnej rzeczywistosci znalazlem kod który bardzo mi się podoba
+
+        while (letsFindMonday.getMonthValue() == month) {
+            if (letsFindMonday.getDayOfWeek() != DayOfWeek.MONDAY) {
+                letsFindMonday = letsFindMonday.plusDays(1);
+                continue;
+            }
+
+            System.out.println(letsFindMonday);
+            letsFindMonday = letsFindMonday.plusDays(7);
+        }
+
+*/
+
 
 
 
