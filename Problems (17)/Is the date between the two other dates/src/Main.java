0508f8a7ecd826951394fe30e0adcf99ab422a84
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-01-24 22:59:17 +0100

    LocalDateTime cd

---
+++ Problems (17)/Is the date between the two other dates/src/Main.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        LocalDate x = LocalDate.parse(sc.next());
+        LocalDate m = LocalDate.parse(sc.next());
+        LocalDate n = LocalDate.parse(sc.next());
+        boolean answer=false;
+
+        if(x.isAfter(m)&& x.isBefore(n)||x.isAfter(n)&& x.isBefore(m)){
+            answer = true;
+        }
+
+        System.out.println(answer);
+
+    }
+}
