f167e398383eec8d4df1012a539be0bec7404698
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-02-14 14:57:47 +0100

    problem of the day

---
+++ Problems (71)/Calculating factorials/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static long factorial(long n) {
+
+        if(n==0||n==1){
+            return 1;
+        }
+        else {
+            return n*factorial(n-1);
+        }
+        // write your code here
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = Integer.parseInt(scanner.nextLine().trim());
+        System.out.println(factorial(n));
+    }
+}
