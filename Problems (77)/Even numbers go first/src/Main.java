02e5e1642c7056e374c69238aded085e851afa4e
grzesiakmarcin <mk.grzesiak@gmail.com>
2021-02-15 15:49:16 +0100

    mistrzostwo swiata

---
+++ Problems (77)/Even numbers go first/src/Main.java
@@ -0,0 +1,33 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        Scanner sc = new Scanner(System.in);
+        Deque<Integer> myQueue = new ArrayDeque<>();
+        int size = sc.nextInt();
+
+
+        while (sc.hasNext()) {
+            int nextInt = sc.nextInt();
+            if (!myQueue.contains(nextInt)) {
+
+                if (nextInt % 2 == 0) {
+                    myQueue.addFirst(nextInt);
+                } else if (nextInt % 2 != 0) {
+                    myQueue.offerLast(nextInt);
+                }
+            }
+
+
+        }
+        //System.out.println(myQueue);
+
+        for (int i=0;i<size;i++){
+            System.out.println(myQueue.poll());
+        }
+
+    }
+}
